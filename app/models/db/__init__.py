0d634e6fc81e5672a9d134cda5269f0889644156
Syricov <syricov50@gmail.com>
2023-04-18 02:22:17 +0300

    Добавлена таблица SGOUser и миграции к ней

--- app/models/db/__init__.py
+++ app/models/db/__init__.py
@@ -1,3 +1,4 @@
 from .base import Base, create_pool
 from .user import User
 from .chat import Chat
+from .sgo_user import SGOUser
---
+++ migrations/versions/b52164d20ace_add_new_table.py
@@ -0,0 +1,32 @@
+"""add new table
+
+Revision ID: b52164d20ace
+Revises: 56df5c6b0df6
+Create Date: 2023-04-18 02:13:03.271206
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'b52164d20ace'
+down_revision = '56df5c6b0df6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'sgo_users',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('tg_id', sa.Integer(), nullable=False),
+        sa.Column('login', sa.String(), nullable=False),
+        sa.Column('password', sa.String(), nullable=False),
+        sa.Column('school', sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint('tg_id'),
+        sa.UniqueConstraint('tg_id')
+    )
+
+
+def downgrade():
+    op.drop_table('sgo_users')
